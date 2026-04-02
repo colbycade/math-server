2ff0f9a92af0bc1ca881ea44cb3d70506630fc29
Temiloluwa <97484828+Temii-mp3@users.noreply.github.com>
2026-04-02 09:36:57 -0500

    added expr evaluator

--- src/ExpressionEvaluator.java
+++ src/ExpressionEvaluator.java
@@ -1,9 +1,76 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+
 
 
 public class ExpressionEvaluator {
-//TBD
+
+    Set<Character> operatorSet = new HashSet<>(Arrays.asList('+', '-', '/', '*', '('));
+
+
+
+
+    public static double evaluate(String expr){
+
+
+
+        return 0.0;
+    }
+
+    public static String formatResult(double r){
+
+
+        return " ";
+    }
+
+    public String convertToPostFix(String eq){
+        StringBuilder infix = new StringBuilder();
+
+        StringBuilder postFix = new StringBuilder();
+
+        Stack<Character> stack = new Stack<>();
+
+        infix.append(eq).append(')');
+
+
+        stack.push('(');
+        for(int i =0; i < infix.length() - 1; i++){
+
+            Character currChar = eq.charAt(i);
+
+            if(currChar.equals(' ')){
+                continue;
+            }
+
+            if(operatorSet.contains(currChar)){
+
+                stack.push(currChar);
+
+            }else if(currChar.equals(')')){
+
+                Character flag;
+
+                while(!(flag = stack.pop()).equals('(')){
+                    postFix.append(flag);
+                }
+            }else{
+
+                postFix.append(currChar);
+
+            }
+        }
+
+
+
+        char flag;
+        while((flag = stack.pop()) != '('){
+            postFix.append(flag);
+        }
+
+        return postFix.toString();
+    }
 }
